3dd109436b2da15caf69dc5f2c928c73a26c6e99
System Administrator <einardavidvillarroel@gmail.com>
2023-11-03 11:18:28 -0400

    Detalles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,13 @@ export class AppComponent {
   description = "No es la historia más bonita, ni la más perfecta. \n Aunque nos conocimos desde siempre, andábamos sin buscarnos pero sabiendo que nos encontraríamos y Dios lo hizo posible. Tuvimos la dicha de enamorarnos y acercarnos a Dios con un propósito. Después de tres años juntos y de vivir las experiencias más bonitas hemos decidido ser más felices y caminar juntos de la mano para siempre.";
   NombreVictor = 'Victor Hugo Adrian';
   descriptionVictor = 'Servicial, fascinado por la música, sociable y dadivoso.';
-  URLFacebookVictor = 'https://www.facebook.com';
-  URLInstagramVictor = 'https://www.facebook.com';
+  URLFacebookVictor = 'https://www.facebook.com/victor.adrian.102';
+  URLInstagramVictor = 'https://instagram.com/victor.adrian?igshid=OGQ5ZDc2ODk2ZA==';
 
   NombreJhu = 'Judith Forco Condori';
   descriptionJhu = 'Creativa, apasionada por viajar, divertida y soñadora.';
-  URLFacebookJhu = 'https://www.facebook.com';
-  URLInstagramJhu = 'https://www.facebook.com';
+  URLFacebookJhu = 'https://www.facebook.com/jhudith.forco';
+  URLInstagramJhu = 'https://instagram.com/jhu_forco?igshid=OGQ5ZDc2ODk2ZA==';
 
   title_Nuestra_Boda = "Nuestra Boda"
   description_Nuestra_Boda = "Con la bendición de nuestros Padres:"

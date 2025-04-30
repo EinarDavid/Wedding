41de68ddc71132c90ae4163122467f1676e7c4ef
System Administrator <einardavidvillarroel@gmail.com>
2025-04-30 16:24:02 -0400

    Interpolación Angular

--- src/app/components/title-description/title-description.component.ts
+++ src/app/components/title-description/title-description.component.ts
@@ -4,10 +4,20 @@ import { Component, Input } from '@angular/core';
   selector: 'app-title-description',
   templateUrl: './title-description.component.html',
   styleUrls: ['./title-description.component.scss', '../../app.component.scss']
+
+
+
 })
 export class TitleDescriptionComponent {
   @Input() Title:string="";
   @Input() Description:string="";
   @Input() EstilosDinamicosTitle:any="";
   @Input() EstilosDinamicosDescription:any="";
+  @Input() Prueba: string='Todo lugar donde sale este texto es un componente';
+  desabilitar=false;
+  mensaje:string='No se ha agregado ninguna persona';
+
+  agregarPersona(){
+    this.mensaje='Persona agregada';
+  }
 }
